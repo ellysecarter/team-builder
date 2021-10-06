72590c04d2986d1e76971519e694b87bb60622f4
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-10-05 20:55:38 -0700

    wrote engineer, intern and manager js files

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,10 +4,13 @@ const Employee = require('../lib/Employee.js')
 
 test('creates a new employee', () => {
     const employee = new Employee('Ellyse');
+
+    const managerEmail = ('test@email.com')
+    const managerEmail = (email)
   
     expect(employee.managerName).toBe('Ellyse');
     expect(employee.managerId).toEqual(expect.any(Number));
-    expect(employee.managerEmail).toBe(expect.any(object))
+    expect(employee.managerEmail).toBe(expect.any(email))
   });
 
 
